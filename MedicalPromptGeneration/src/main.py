6aebe633490be6a22a003d8f8402a789c4a4c604
jeix H <jeix782@gmail.com>
2024-04-24 23:01:31 +0800

    delete T2T

--- MedicalPromptGeneration/src/main.py
+++ MedicalPromptGeneration/src/main.py
@@ -3,7 +3,7 @@ import json
 from model import VQAModel
 from PIL import Image
 import os
-from transformers import pipeline
+import re
 
 def process_questions(model, image, questions):
     answers = []
@@ -13,6 +13,11 @@ def process_questions(model, image, questions):
             answer = model.answer_question(image, question)
             answers.append({"question": question, "answer": answer})
     return answers
+
+def extract_keywords(text):
+    # 这里使用了简单的正则表达式来匹配名词短语作为关键词，这个方法可以根据需要进行更复杂的改进
+    keywords = re.findall(r'\b\w+\b', text)
+    return keywords
 
 def main():
     parser = argparse.ArgumentParser(description="Generate a list of attributes from images using different VQA models.")
@@ -40,25 +45,16 @@ def main():
         questions = file.readlines()
     imv_answers = process_questions(model_imv, image, questions)
 
-    # Combine answers
-    combined_answers = blip_answers + imv_answers
+    # Extract keywords and save
+    keywords = []
+    for answer in blip_answers + imv_answers:
+        keywords.extend(extract_keywords(answer['answer']))
 
-    # Prepare document QA pipeline
-    nlp = pipeline("document-question-answering", model="naver-clova-ix/donut-base-finetuned-docvqa", tokenizer="naver-clova-ix/donut-base-finetuned-docvqa")
-
-    # Extract relevant information
-    extracted_info = []
-    for answer_dict in combined_answers:
-        response = nlp(answer_dict["answer"], answer_dict["question"])
-        extracted_info.append(response["answer"])
-
-    # Save and print the results
     output_path = os.path.join(os.path.dirname(__file__), '..', 'outputs', 'ret.txt')
     with open(output_path, 'w') as f:
-        for info in extracted_info:
-            f.write(info + "\n")
+        f.write("\n".join(keywords))
 
-    print("\n".join(extracted_info))
+    print("\n".join(keywords))
 
 if __name__ == "__main__":
     main()
